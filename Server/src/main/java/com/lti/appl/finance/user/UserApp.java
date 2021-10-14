6dd2027fd9b5aa9b184543d6f2e05541aa876f64
Aravind <araviku04@gmail.com>
2021-10-14 17:38:54 +0530

    added server template code

---
+++ Server/src/main/java/com/lti/appl/finance/user/UserApp.java
@@ -0,0 +1,71 @@
+package com.lti.appl.finance.user;
+
+//import java.util.List;
+//import java.util.Scanner;
+//
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+//import com.lti.appl.casestudy.beans.Employee;
+//import com.lti.appl.casestudy.services.EmpService;
+//import com.lti.appl.casestudy.services.EmpServiceImpl;
+
+public class UserApp {
+
+	public static void main(String[] args) {
+		
+//		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+//
+//		EmpService service = context.getBean("empService", EmpServiceImpl.class);
+//
+//		List<Employee> clientList = service.getEmpList();
+//
+//		System.out.println(clientList);
+//
+//		int id;
+//		String name;
+//		float sal;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter empno : ");
+//		id = sc.nextInt();
+//		sc.nextLine();
+//		System.out.println("Enter emp name : ");
+//		name = sc.nextLine();
+//		System.out.println("Enter emp sal : ");
+//		sal = sc.nextFloat();
+//
+//		Employee e = new Employee(name, sal);
+
+		//insert
+//		id=service.addEmployee(e);
+//		System.out.println("Your emp no is : "+id);
+		
+		
+		//search
+//		System.out.println("Enter id to search : ");
+//		id=sc.nextInt();
+//		Employee e=service.searchById(id);
+//		
+//		if(e!=null)
+//			System.out.println("Record found : "+e);
+//		else
+//			System.out.println("Record not found");
+		
+		//delete
+//		service.deleteEmployee(21);
+		
+		//update
+//		System.out.println("Enter id to update : ");
+//		id=sc.nextInt();
+//		sc.nextLine();
+//		System.out.println("Enter name to update : ");
+//		name=sc.nextLine();
+//		System.out.println("Enter salary to update : ");
+//		sal=sc.nextFloat();
+//		service.updateEmployee(id,name,sal);
+		
+		
+//		sc.close();
+	}
+
+}
